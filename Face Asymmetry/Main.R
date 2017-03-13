40a0b1434010b33566db5647ac2b9f97359fcc64
DTanJP <DTanJP@gmail.com>
2017-03-13 00:04:23 -0400

    Add files via upload

--- Face Asymmetry/Main.R
+++ Face Asymmetry/Main.R
@@ -22,6 +22,7 @@ function.faceAsymmetry <- function(filename = NA, axisSearch=0, xmin = NA, xmax 
   image.face = readJPEG(filename)
   dim(image.face)
   
+  image.face = ((image.face[,,1]+image.face[,,2]+image.face[,,3])/3)
   #If there is no delimiters set, default should be the entire image
   if(is.na(xmin)) xmin = 1
   if(is.na(xmax)) xmax = dim(image.face)[1]
@@ -43,35 +44,41 @@ function.faceAsymmetry <- function(filename = NA, axisSearch=0, xmin = NA, xmax 
   vector.axis = (symmetry.axis - axisSearch) : (symmetry.axis + axisSearch)
   
   #Applying the equation onto the image
+  for(axis in vector.axis) {
+    result <- 0
   for(y in ymin:ymax) {
-    for(x in vector.axis) {#Loop through all the values that will be tried
+    for(x in xmin:xmax) {#Loop through all the values that will be tried
       
       #Get the point to the left side of the symmetry axis
-      point1 = function.intensityValue(image.face, x, y)
+      #point1 = function.intensityValue(image.face, x, y)
+      point1 = image.face[x,y]
       
       #Get the point to the right side of the symmetry axis
-      point2 = function.intensityValue(image.face, (x + (axisSearch * 2)), y)
+      #point2 = function.intensityValue(image.face, (x + (axisSearch * 2)), y)
+      point2 = image.face[(2 * axis) - x, y]
       
       #Calculate the symmetry
-      result = function.equation(point1, point2)
-      
-      #Find the greatest symmetry value
-      if(num.symmetryMax < result) {
-        num.symmetryMax = result
-        num.axisMax = x
-      }
+      #result = function.equation(point1, point2)
+      result = (result + ((point1 - point2)^2))
       
       #DEBUG
       string.debug = paste(x, (x + (axisSearch * 2)), function.equation(point1, point2), sep = "    ")
       print(string.debug)
-      
-      #Append the calculated value onto the vector
-      vector.asymmetry <- c(vector.asymmetry, result)
     }
     #DEBUG
     print("")
   }
-  
+    result = sqrt(result)
+    
+    #Find the greatest symmetry value
+    if(num.symmetryMax < result) {
+      num.symmetryMax = result
+      num.axisMax = x
+    }
+    
+    #Append the calculated value onto the vector
+    vector.asymmetry <- c(vector.asymmetry, result)
+  }
   #Return the results
   return (list(vector.axis, vector.asymmetry, num.axisMax, num.symmetryMax))
 }
