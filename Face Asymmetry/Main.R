44770ccd3cad893966add59ddbcab444c54f8f09
DTanJP <DTanJP@gmail.com>
2017-03-23 16:01:32 -0400

    Add files via upload

--- Face Asymmetry/Main.R
+++ Face Asymmetry/Main.R
@@ -14,31 +14,34 @@ function.faceAsymmetry <- function(filename = NA, axisSearch=0, xmin = NA, xmax 
   #Create empty vector variable
   vector.asymmetry = c()
   #The axis that has the greatest symmetry value
-  num.axisMax = 0
+  num.axisMax = as.numeric(0)
   #The greatest symmetry value
-  num.symmetryMax = 0
+  num.symmetryMax = as.numeric(0)
+  #The result calculated
+  result = as.numeric(0)
   
   #Read the image
   image.face = readJPEG(filename)
   dim(image.face)
   
+  #Conver the image to grayscale
   image.face = ((image.face[,,1]+image.face[,,2]+image.face[,,3])/3)
-  #If there is no delimiters set, default should be the entire image
-  if(is.na(xmin)) xmin = 1
-  if(is.na(xmax)) xmax = dim(image.face)[1]
-  if(is.na(ymin)) ymin = 1
-  if(is.na(ymax)) ymax = dim(image.face)[2]
   
-  #Check to make sure xmax is within bounds of the image
-  if(xmax > dim(image.face)[1])
+  #Decide the correct limits
+  if(is.na(xmin) || xmin < 1) 
+    xmin = 1
+  
+  if(is.na(xmax) || xmax > dim(image.face)[1])
     xmax = dim(image.face)[1]
   
-  #Check to make sure ymax is within bounds of the image
-  if(ymax > dim(image.face)[2])
+  if(is.na(ymin) || ymin < 1) 
+    ymin = 1
+  
+  if(is.na(ymax) || ymax > dim(image.face)[2]) 
     ymax = dim(image.face)[2]
   
-  #Find the symmetry line
-  symmetry.axis = xmin + ((xmax - xmin)/2)
+  #Find the symmetry line(xmin inclusive)
+  symmetry.axis = xmin + ((xmax - (xmin+1))/2)
   
   #Save the x axis values that will be used in the calculations
   vector.axis = (symmetry.axis - axisSearch) : (symmetry.axis + axisSearch)
@@ -49,50 +52,48 @@ function.faceAsymmetry <- function(filename = NA, axisSearch=0, xmin = NA, xmax 
   for(y in ymin:ymax) {
     for(x in xmin:xmax) {#Loop through all the values that will be tried
       
-      #Get the point to the left side of the symmetry axis
-      #point1 = function.intensityValue(image.face, x, y)
-      point1 = image.face[x,y]
+      distance.x1 = (axis - x) #The distance from x - axis(Left - midline)
+      distance.x2 = (((2 * axis) - x) - axis) #The distance from axis to x2(Midline - right)
+      distance.x = min(distance.x1, distance.x2) #The min distance from the axis
       
-      #Get the point to the right side of the symmetry axis
-      #point2 = function.intensityValue(image.face, (x + (axisSearch * 2)), y)
-      point2 = image.face[(2 * axis) - x, y]
-      
-      #Calculate the symmetry
-      #result = function.equation(point1, point2)
-      result = (result + ((point1 - point2)^2))
-      
-      #DEBUG
-      string.debug = paste(x, (x + (axisSearch * 2)), function.equation(point1, point2), sep = "    ")
-      print(string.debug)
+      #Pick the x values that are equal in both sides + inside the boundaries of the image
+      if(distance.x > 0) {
+        if((axis - distance.x) > 0 && (axis + distance.x) < dim(image.face)[1]) {
+        #DEBUG
+        #print(paste(distance.x1, distance.x2, distance.x, sep = "   "))
+        
+        #Get the point to the left side of the symmetry axis
+        #point1 = image.face[x,y]
+        point1 = image.face[axis - distance.x, y]
+        
+        #Get the point to the right side of the symmetry axis
+        #point2 = image.face[((2 * axis) - x), y]
+        point2 = image.face[axis + distance.x, y]
+        
+        #Calculate the symmetry
+        result <- (result + ((point1 - point2)^2))
+        
+        #DEBUG
+        #print(paste(distance.x, point1, point2, sep = "     "))
+        string.debug = paste(x, axis, ((axis * 2) - x), ((point1 - point2)^2), (" || "), sep = "  ~  ")
+        print(string.debug)
+        }
+      }
     }
     #DEBUG
-    print("")
-  }
-    result = sqrt(result)
+    #print("")
     
-    #Find the greatest symmetry value
-    if(num.symmetryMax < result) {
-      num.symmetryMax = result
-      num.axisMax = x
     }
-    
-    #Append the calculated value onto the vector
-    vector.asymmetry <- c(vector.asymmetry, result)
   }
   #Return the results
   return (list(vector.axis, vector.asymmetry, num.axisMax, num.symmetryMax))
 }
 
-#Calculates the intensity value of f(x,y) by averaging the rgb values
-function.intensityValue <- function(image.face, x, y) {
-  return((image.face[x,y,1]+image.face[x,y,2]+image.face[x,y,3])/3)
-}
-
-#Calculates the asymmetry using 2 points(Intensity values)
-function.equation <- function(intensity.point1, intensity.point2) {
-  result = ((intensity.point1 - intensity.point2)^2)
-}
+#Converts a pixel to grayscale
+function.intensityValue <- function(x, y, array.image) {
+  return ((array.image[x,y,1] + array.image[x,y,2] + array.image[x,y,3])/3)
+} 
 
 #List of graph data
-list.graphVariables <- function.faceAsymmetry("face1.jpg",10,940,1130,740,880)
+list.graphVariables <- function.faceAsymmetry("face1.jpg",3,1,10,1,10)
 print("Done")
